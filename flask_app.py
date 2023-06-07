c4a291e20a32dd6602c15fb211198be6028edce5
Aditya Shinde <48020262+adi12lc@users.noreply.github.com>
2023-06-07 17:45:20 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,72 +1,73 @@
+# Importing necessary libraries
 from sentence_transformers import SentenceTransformer
 from flask import Flask, request, render_template
-import pandas as pd
 from google.cloud import bigquery
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
+# Initializing Flask app
 app = Flask(__name__)
 app.config["HOST"] = "0.0.0.0"
 app.config["PORT"] = 8080
 
-# Initialize SentenceTransformer model
+# Loading the SentenceTransformer model
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-def get_bigquery_data():
-            
-    # Create a BigQuery client
-    client = bigquery.Client()
+# Creating a class for managing and storing embeddings
+class Embeddings:
+    def __init__(self, query, model):
+        # Initialize class with a SQL query and the sentence-transformer model
+        self.query = query
+        self.client = bigquery.Client()
+        self.model = model
+        self.update_embeddings()  # Update embeddings upon initialization
 
-    # Define your BigQuery SQL query
-    query =  " SELECT * FROM `newprojv1.clothadi.clothsearch` "
+    def get_bigquery_data(self):
+        # Run a BigQuery job, get the results, and convert to a DataFrame
+        query_job = self.client.query(self.query)
+        self.df = query_job.to_dataframe()
 
-    # Execute the query
-    query_job = client.query(query)
+    def update_embeddings(self):
+        # Update embeddings by fetching the latest data and computing their embeddings
+        self.get_bigquery_data()
+        descriptions = self.df['detail_description'].tolist()
+        self.df['embeddings'] = list(self.model.encode(descriptions))
 
-    # Fetch the results as a pandas DataFrame
-    df = query_job.to_dataframe()
+    def get_similar_items(self, description, n=5):
+        # Get the n most similar items to a given description
+        # Compute the cosine similarity between the provided description and stored embeddings
+        user_embedding = self.model.encode([description])
+        self.df['similarity'] = cosine_similarity(np.vstack(self.df['embeddings'].values), user_embedding)
+        top_indexes = self.df['similarity'].argsort()[-n:][::-1]  # Get the indices of top-n similar items
+        recommendations = self.df.loc[top_indexes, 'title'].tolist()  # Get the titles of recommended items
+        links = self.df.loc[top_indexes, 'URL'].tolist()  # Get the URLs of recommended items
+        return recommendations, links
 
-    return df
+# SQL query to fetch data
+query = "SELECT * FROM `newprojv1.clothadi.clothsearch`"
+# Instantiate the EmbeddingStorage class
+storage = Embeddings(query, model)
 
-# Read the CSV file from the google cloud bucket 
-df = get_bigquery_data()
-
-# Encode detail descriptions
-df['embeddings'] = df['detail_description'].apply(lambda x: model.encode(x))
-
-# Route for recommendation
+# Define a route for the recommendation system
 @app.route('/', methods=['GET', 'POST'])
 def recommend():
-    # Initialize recommendation and link lists
+    # Initialize variables
     recommendations = [None] * 5
     links = [None] * 5
     user_description = "Description"
-    index = []
+    index = list(range(1, 6))
 
     if request.method == 'POST':
-        # Get user description from form
+        # Get user's description from the form
         user_description = request.form.get('description')
+        # Fetch recommended items and their links based on user's description
+        recommendations, links = storage.get_similar_items(user_description)
 
-        # Encode user description
-        user_embedding = model.encode(user_description)
-
-        # Calculate similarity between user embedding and each detail description embedding
-        df['similarity'] = cosine_similarity(np.vstack(df['embeddings'].values), [user_embedding])
-
-        # Get the indexes of top 5 similar embeddings
-        top_indexes = df['similarity'].argsort()[-5:][::-1]
-
-        # Get the top 5 recommendations and their links
-        recommendations = df.loc[top_indexes, 'title'].tolist()
-        links = df.loc[top_indexes, 'URL'].tolist()
-
-        # for indexing the items
-        for i in range(1,6):
-            index.append(i)
-
+    # Render the results on the webpage
     return render_template('index.html', links=links, recommendations=zip(recommendations, links, index), description=user_description)
 
 
+# Run the Flask application
 if __name__ == '__main__':
     app.run(host=app.config["HOST"], port=app.config["PORT"])
 
